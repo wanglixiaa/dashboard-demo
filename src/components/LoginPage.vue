6e0a76e83d8eb897b0951b3640991aaa083f3663
Reficul <xuzhenglun@gmail.com>
2018-09-12 11:11:42 +0800

    初始化项目”

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>nnn</div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import LoginPage from '@/components/LoginPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      component: LoginPage
+    }
+  ]
+})
