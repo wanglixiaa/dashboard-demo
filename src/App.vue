9931b38f21ab3e3aaf81395b5df3aa304332ae74
wanglixiaa <1018482705@qq.com>
2018-09-12 23:46:22 +0800

    布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <navmenu></navmenu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 
 <script>
+
+import NavMenu from '@/components/NavMenu'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    'navmenu': NavMenu
+  }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
